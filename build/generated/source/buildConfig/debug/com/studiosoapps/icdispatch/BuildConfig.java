7ecd69e12e2f7c55363576d6a6220c20b4431319
Charlie Porth <charlieporth1@gmail.com>
2019-12-31 21:04:52 -0600

    FUKC ALL FUCKS

--- build/generated/source/buildConfig/debug/com/studiosoapps/icdispatch/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/studiosoapps/icdispatch/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.risch.Evertsson.icdispatch;
+package com.studiosoapps.icdispatch;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "com.risch.Evertsson.icdispatch";
+  public static final String LIBRARY_PACKAGE_NAME = "com.studiosoapps.icdispatch";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
