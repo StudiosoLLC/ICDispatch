c4ae373a0fbde39755e259c8f5c6b862db730913
Charlie Porth <charlieporth1@gmail.com>
2020-02-17 18:23:36 -0600

    Added prog thing

--- build/generated/source/buildConfig/release/com/studiosoapps/icdispatch/BuildConfig.java
+++ build/generated/source/buildConfig/release/com/studiosoapps/icdispatch/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String LIBRARY_PACKAGE_NAME = "com.studiosoapps.icdispatch";
   public static final String BUILD_TYPE = "release";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "5.9";
 }
