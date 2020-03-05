8384c11c9f328b2813e37e9fec21b5eca07e8228
Charlie Porth <charlieporth1@gmail.com>
2020-03-05 16:40:10 -0600

    Added tech filter remote shit

--- src/main/java/com/studiosoapps/icdispatch/ICForkJoinQueue.java
+++ src/main/java/com/studiosoapps/icdispatch/ICForkJoinQueue.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Studioso Inc if you are in positon of this material with proper explicit permissions for Studioso Inc then you must destroy this property on your machine
+ */
+
 package com.studiosoapps.icdispatch;
 
 import java.util.concurrent.BlockingQueue;
